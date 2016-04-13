68296f8870d4f0cf8e06581eb66387130bb7ce24
Joe Lencioni <joe.lencioni@airbnb.com>
2016-04-13 09:45:04 -0700

    Change Object.entries and Object.values to stage 4
    These were recently moved to stage 4 at the most recent TC39 and will be
    included in ES 2017.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('es6-shim');
 // Array#includes is stage 4, in ES7/ES2016
 require('array-includes/shim')();
 
-// Object.values/Object.entries are stage 3, planned for ES8/ES2017
+// Object.values/Object.entries are stage 4, in ES8/ES2017
 require('object.values/shim')();
 require('object.entries/shim')();
 
