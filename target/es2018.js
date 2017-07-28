60e966b640febc228bd1fad8a0601963b5020b22
Jordan Harband <ljharb@gmail.com>
2017-07-28 15:07:20 -0700

    [Dev Deps] update `eslint`, `eslint-config-airbnb-base`

--- target/es2018.js
+++ target/es2018.js
@@ -2,5 +2,5 @@
 
 // Promise.prototype.finally is stage 3, a possible candidate for ES2018
 if (typeof Promise === 'function') {
-  require('promise.prototype.finally/shim')();
+  require('promise.prototype.finally/shim')(); // eslint-disable-line global-require
 }
